91ea56a90a676dee2f509d39affbb67862aa0f84
sam-christensen <chr11003@byui.edu>
2014-03-12 16:58:54 -0600

    New Code

---
+++ src/com/example/firstapp/CrapCode.java
@@ -0,0 +1,8 @@
+/* android:layout_centerHorizontal="true"
+        android:layout_centerInParent="true"
+        android:layout_centerVertical="true"
+        android:layout_marginTop="15sp"
+        android:layout_width="150dp"
+        android:layout_height="225dp"
+        android:layout_width="150dp"
+        android:layout_height="150dp"*/
